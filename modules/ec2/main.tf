18487be9918d65e5935802db77aeb3c98220edde
tayyab-cloud <https://github.com/tayyab-cloud>
2025-12-07 02:46:23 +0500

    fix(ec2): set vpc_security_group_ids from var.security_group_ids

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -4,7 +4,8 @@ resource "aws_instance" "my-instance" {
     ami           = var.ami-id
     instance_type = var.instance-type
     key_name      = var.key_name
-    vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
+    vpc_security_group_ids = var.security_group_ids
+    
 
     
     tags = {
